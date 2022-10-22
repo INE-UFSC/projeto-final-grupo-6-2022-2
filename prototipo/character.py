8aa88a4fe1cf0eb32ae0e5594ccfbb16c27256ad
jresmer <jpresmer@gmail.com>
2022-10-22 12:08:15 -0300

    adicionada var sprite (tratamento de excecoes pendente)

--- prototipo/character.py
+++ prototipo/character.py
@@ -1,16 +1,19 @@
 from abc import ABC, abstractmethod
 from math import sqrt
+import pygame
 
 
 class Character(ABC):
 
-    def __init__(self, size: int, health: int, pos: tuple, speed: int):
+    def __init__(self, size: int, health: int, pos: tuple, speed: int, sprite: str):
         self.__size = size
         self.__health = health
         self.__speed = speed
         self.__diagonal_speed = speed/sqrt(2)
         self.__posx = pos[0]
         self.__posy = pos[1]
+        # PLANEJAR COMO LIDAR COM EXCECOES:
+        self.__sprite = self.image = pygame.image.load(sprite).convert_alpha()
 
     def getPos(self):
         return (self.__posx, self.__posy)
