12119e9bf9b52dc2b6bcce387bae72c4b2b35079
jresmer <jpresmer@gmail.com>
2022-10-22 17:14:42 -0300

    atualizado

--- prototipo/enemy.py
+++ prototipo/enemy.py
@@ -5,7 +5,7 @@ from abc import abstractmethod
 class Enemy(Character):
 
     def __init__(self, size: int, health: int, pos: tuple, speed: int, sprite: str, damage: int):
-        super().__init__(size: int, health: int, pos: tuple, speed: int, sprite: str)
+        super().__init__(size, health, pos, speed, sprite)
         self.__awake = False
 
     @abstractmethod
