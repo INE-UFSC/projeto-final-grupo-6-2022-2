8e73c3652039a7e64d168a81e66ac3f84ede0c2d
Guilhermeaj19 <guilhermeadenilson16@gmail.com>
2022-11-02 14:24:04 -0300

    Testando draw da lanterna

--- prototipo/lanterna.py
+++ prototipo/lanterna.py
@@ -3,30 +3,30 @@ import jogador
 from pilha import Pilha
 
 
-class Lanterna:
-    def __init__(self, x, y, cor, tamanho, group, status = False, pilha = None, tempo_ligada = 0):
-        self.__x = x
-        self.__y = y
-        self.cor = cor
-        self.tamanho = tamanho
+class Lanterna(pygame.sprite.Sprite):
+    def __init__(self, pos, status = False, pilha = None, tempo_ligada = 0):
+        super().__init__(pygame.sprite.Group())
+        self.__x = pos[0]
+        self.__y = pos[1]
+        self.cor = (0, 255, 255)
         self.__status = status
         self.pilha = pilha
         self.tempo_ligada = tempo_ligada
-        self.image = pygame.image.load('tiles/lanterna.png')
-        self.rect = self.image.get_rect(topleft = (self.x, self.y))
-        self.hitbox = self.rect.inflate(0,-26)
+        
+        self.image = pygame.image.load('tiles/rock.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = (self.__x, self.__y))
+        #self.hitbox = self.rect.inflate(0,-26)
      
     def setPos(self, x, y):
-        self.__x = x
-        self.__y = y
+        self.__x = 640-32
+        self.__y = 360-19
     
     def getStatus(self):
         return self.__status
 
-    def setStatus(self, status: bool):
-        self.__status = status
-    
-    def draw(self, pos, tela):
-        pygame.draw.rect(tela, self.cor, (pos[0], pos[1], self.tamanho[0], self.tamanho[1]))
+    def setStatus(self):
+        self.__status = not self.__status
 
-
+    def draw(self, tela):
+        if self.__status:
+            tela.blit(self.image, (640-32, 360-19))
