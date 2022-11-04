e9787d4ad2f8ac781915c56150c3ca7af47d00bc
jresmer <jpresmer@gmail.com>
2022-11-03 22:12:18 -0300

    versao de exeplo da arma

--- prototipo/weapon.py
+++ prototipo/weapon.py
@@ -2,9 +2,16 @@ from item import Item
 
 
 class Weapon(Item):
-    def __init__(self, x, y, sprite, grupo, damage):
+    def __init__(self, x, y, sprite, grupo, damage, range):
         super().__init__(self, x, y, sprite,grupo)
         self.__damage = damage
+        self.__range = range
 
     def use(self, player):
-        pass
+        player.setWeapon(self)
+
+    def getRange(self):
+        return self.__range
+
+    def getDamage(self):
+        return self.__damage
