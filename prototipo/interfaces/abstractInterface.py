e8a8fa9d00b2216bae823a6f8fd751cd36522ae2
Guilhermeaj19 <guilhermeadenilson16@gmail.com>
2022-11-22 08:31:41 -0300

    Privando atributos da classe abstrata de Interface

--- prototipo/interfaces/abstractInterface.py
+++ prototipo/interfaces/abstractInterface.py
@@ -3,9 +3,15 @@ from abc import ABC, abstractmethod
 
 class AbstractInterface(ABC):
     def __init__(self, screen, file_background_image):
-        self.screen = screen
-        self.background = pygame.image.load(file_background_image)
+        self.__screen = screen
+        self.__background = pygame.image.load(file_background_image)
 
+    def getScreen(self):
+        return self.__screen
+    
+    def getBackground(self):
+        return self.__background
+    
     @abstractmethod
     def start(self):
         pass
--- prototipo/interfaces/controlsInterface.py
+++ prototipo/interfaces/controlsInterface.py
@@ -6,7 +6,7 @@ from button import Button
 class ControlsInterface(AbstractInterface):
     def __init__(self, screen, file_background_image):
         super().__init__(screen, file_background_image)
-        self.buttons = pygame.sprite.Group([Button(420, 90, 'prototipo\interfaces\setaControleMouseSelecionado.png', 'prototipo\interfaces\setaControleSemMouse.png')])
+        self.__buttons = pygame.sprite.Group([Button(420, 90, 'prototipo\interfaces\setaControleMouseSelecionado.png', 'prototipo\interfaces\setaControleSemMouse.png')])
     
     def start(self, clock):
         run = True
@@ -17,7 +17,7 @@ class ControlsInterface(AbstractInterface):
                 if event.type == pygame.QUIT:
                     run = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    for button in self.buttons:
+                    for button in self.__buttons:
                         if event.button == 1 and button.colliding():
                             print('voltar para janela anterior')
                             return
@@ -26,13 +26,13 @@ class ControlsInterface(AbstractInterface):
             pygame.display.flip()
     
     def draw(self):
-        self.buttons.update()
-        self.screen.fill((0,0,0))
-        self.screen.blit(self.background, ((1280-self.background.get_rect()[2])//2,0))
-        self.buttons.draw(self.screen)
-"""        
+        self.__buttons.update()
+        self.getScreen().fill((0,0,0))
+        self.getScreen().blit(self.getBackground(), ((1280-self.getBackground().get_rect()[2])//2,0))
+        self.__buttons.draw(self.getScreen())
+       
 pygame.init()
 window = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 tela = ControlsInterface(window, 'prototipo\interfaces\Captura de tela_20221104_205804.png')
-tela.start(clock)"""
+tela.start(clock)
