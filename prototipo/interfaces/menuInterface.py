02b16fd2f88ffb57946c1d60d4078a7c09c7a245
Guilhermeaj19 <guilhermeadenilson16@gmail.com>
2022-11-24 18:47:02 -0300

    Ajustes na interface de menu

--- prototipo/interfaces/menuInterface.py
+++ prototipo/interfaces/menuInterface.py
@@ -1,27 +1,28 @@
 import pygame
+import sys
 
 from abstractInterface import AbstractInterface
-#from optionsInterface import OptionsInterface
+from optionsInterface import OptionsInterface
 from button import Button
 
 class MenuInterface(AbstractInterface):
     def __init__(self):
         super().__init__(pygame.display.get_surface(), 'interfaces\menu_jogo.png')
         self.__buttons = pygame.sprite.Group(
-            [Button(1030, 375, 'interfaces\Botoes\\botao_start_hover.png',
+            [Button(1030, 350, 'interfaces\Botoes\\botao_start_hover.png',
                              'interfaces\Botoes\\botao_start.png', 
                              'interfaces\Botoes\\botao_start_pressed.png', 'start')],
-            [Button(1030, 425, 'interfaces\Botoes\\botao_continue_hover.png',
+            [Button(1030, 450, 'interfaces\Botoes\\botao_continue_hover.png',
                              'interfaces\Botoes\\botao_continue.png', 
                              'interfaces\Botoes\\botao_continue_pressed.png', 'continue')],
-            [Button(1030, 475, 'interfaces\Botoes\\botao_options_hover.png',
+            [Button(1030, 550, 'interfaces\Botoes\\botao_options_hover.png',
                              'interfaces\Botoes\\botao_options.png', 
                              'interfaces\Botoes\\botao_options_pressed.png', 'options')],
-            [Button(1030, 525, 'interfaces\Botoes\\botao_exit_hover.png',
+            [Button(1030, 650, 'interfaces\Botoes\\botao_exit_hover.png',
                              'interfaces\Botoes\\botao_exit.png', 
                              'interfaces\Botoes\\botao_exit_pressed.png', 'exit')])
 
-        #self.__options = OptionsInterface()
+        self.__options = OptionsInterface()
         
     def start(self, clock):
         run = True
@@ -29,13 +30,14 @@ class MenuInterface(AbstractInterface):
             clock.tick(60)
             events = pygame.event.get()
             for event in events:
-                
                 if event.type == pygame.QUIT:
-                    run = False
+                    pygame.quit()
+                    sys.exit()
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     for button in self.__buttons:
                         if event.button == 1 and button.colliding():
-                            return
+                            if button.key == 'options':
+                                self.__options.start(clock)
                         
             self.draw()
             pygame.display.flip()
