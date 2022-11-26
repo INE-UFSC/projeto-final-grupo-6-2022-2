8e918d724ee951cf830691e67f871afba00c528e
jresmer <112180846+jresmer@users.noreply.github.com>
2022-11-26 18:13:06 -0300

    recommit

--- prototipo/enemy.py
+++ prototipo/enemy.py
@@ -1,17 +1,19 @@
 from character import Character
 from abc import abstractmethod
+from damageController import DamageController
 
 
 class Enemy(Character):
-
     def __init__(self, health: int, pos: tuple, speed: int, sprite: str, damage: int, obstacle_sprites, player):
         super().__init__(health, pos, speed, sprite, obstacle_sprites)
         self.__player = player
         self.__awake = False
         self.__damage = damage
+        self.__range = 400
 
-    def attack(self, player):
-        player.receiveDamage(self.__damage)
+    def attack(self):
+        dmg_ctrl = DamageController()
+        dmg_ctrl.meele_attack(self.__damage, self.__range, str(self))
 
     def collision(self, direction):
         if direction == 'horizontal':
@@ -21,7 +23,6 @@ class Enemy(Character):
                         self.hitbox.right = sprite.hitbox.left
                     if self.getDirectionX() < 0: # Se mover para a esquerda
                         self.hitbox.left = sprite.hitbox.right
-
         if direction == 'vertical':
             for sprite in (list(self.getObstacleSprites()) + [self.__player]):
                 if sprite.hitbox.colliderect(self.hitbox):
@@ -30,15 +31,19 @@ class Enemy(Character):
                     if self.getDirectionY() < 0: # Se mover para cima
                         self.hitbox.top = sprite.hitbox.bottom
 
+    def __str__(self):
+        return 'enemy'
+
     def update(self):
         self.cooldowns()
         self.reactToLight()
         self.move(self.getSpeed())
-
     @abstractmethod
     def reactToLight(self):
         pass
-    
+
     def getPlayer(self):
         return self.__player
-    
+
+    def getRange(self):
+        return self.__range
