602af58d4a0bf1d888adaeaac7b23dbd2e05377e
jresmer <112180846+jresmer@users.noreply.github.com>
2022-11-26 18:12:06 -0300

    recommit

--- prototipo/damageController.py
+++ prototipo/damageController.py
@@ -1,19 +1,19 @@
 from singletonMeta import SingletonMeta
 from math import sqrt
-
-
 class DamageController(metaclass=SingletonMeta):
-
     def __init__(self):
         self.__enemies = None
         self.__player = None
-
     def update_characters(self, enemies, player):
         self.__enemies = enemies
         self.__player = player
 
-    def meele_attack(self, damage, attack_range):
-        for enemy in self.__enemies:
+    def meele_attack(self, damage, attack_range, enemy_sel='enemies'):
+        if enemy_sel == 'enemies':
+            enemies = self.__enemies
+        else:
+            enemies = [self.__player]
+        for enemy in enemies:
             x, y = enemy.getPos()
             x1, y1 = self.__player.getPos()
             diffx = x - x1
@@ -29,3 +29,4 @@ class DamageController(metaclass=SingletonMeta):
     def projectile_damage(self, projectile, enemy):
         dmg = projectile.getDamage()
         # enemy.receiveDamage(dmg)
+        
