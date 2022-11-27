ba3f891d2fb488236c36960db8aebc1fba21883a
Guilhermeaj19 <guilhermeadenilson16@gmail.com>
2022-11-27 20:13:42 -0300

    Mudando a implementação da tela de Controles

--- prototipo/interfaces/menuInterface.py
+++ prototipo/interfaces/menuInterface.py
@@ -1,28 +1,33 @@
 import pygame
 import sys
 
-from abstractInterface import AbstractInterface
-from optionsInterface import OptionsInterface
+from interfaces.abstractInterface import AbstractInterface
+from interfaces.optionsInterface import OptionsInterface
+from interfaces.controlsInterface import ControlsInterface
 from button import Button
 
 class MenuInterface(AbstractInterface):
     def __init__(self):
         buttons = pygame.sprite.Group(
-            [Button(1030, 350, 'interfaces\Botoes\\botao_start_hover.png',
+            [Button(1030, 250, 'interfaces\Botoes\\botao_start_hover.png',
                              'interfaces\Botoes\\botao_start.png', 
                              'interfaces\Botoes\\botao_start_pressed.png', 'start')],
-            [Button(1030, 450, 'interfaces\Botoes\\botao_continue_hover.png',
+            [Button(1030, 350, 'interfaces\Botoes\\botao_continue_hover.png',
                              'interfaces\Botoes\\botao_continue.png', 
                              'interfaces\Botoes\\botao_continue_pressed.png', 'continue')],
-            [Button(1030, 550, 'interfaces\Botoes\\botao_options_hover.png',
+            [Button(1030, 450, 'interfaces\Botoes\\botao_options_hover.png',
                              'interfaces\Botoes\\botao_options.png', 
                              'interfaces\Botoes\\botao_options_pressed.png', 'options')],
+            [Button(1030, 550, 'interfaces\Botoes\\botao_controls_hover.png',
+                             'interfaces\Botoes\\botao_controls.png', 
+                             'interfaces\Botoes\\botao_controls_pressed.png', 'controls')],
             [Button(1030, 650, 'interfaces\Botoes\\botao_exit_hover.png',
                              'interfaces\Botoes\\botao_exit.png', 
                              'interfaces\Botoes\\botao_exit_pressed.png', 'exit')])
         
         super().__init__(pygame.display.get_surface(), 'interfaces\menu_jogo.png', buttons)
         self.__options = OptionsInterface()
+        self.__controls = ControlsInterface()
 
     def update(self):
         if self.getChangeInterface():
@@ -32,6 +37,8 @@ class MenuInterface(AbstractInterface):
             elif key == 'exit':
                 pygame.quit()
                 sys.exit()
+            elif key == 'controls':
+                self.__controls.start(pygame.time.Clock())
             elif key == 'continue' or key == 'start':
                 return key
                 
@@ -42,9 +49,3 @@ class MenuInterface(AbstractInterface):
         self.getScreen().fill((0,0,0))
         self.getScreen().blit(self.getBackground(), ((1280-self.getBackground().get_rect()[2])//2,0))
         self.getButtons().draw(self.getScreen())
-
-pygame.init()
-window = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-tela = MenuInterface()
-tela.start(clock)
