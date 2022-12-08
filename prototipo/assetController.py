dc862c65deb48787ba32c29ea996ba8b7d6492ec
jresmer <jpresmer@gmail.com>
2022-12-08 10:53:11 -0300

    classe criada

---
+++ prototipo/assetController.py
@@ -0,0 +1,29 @@
+import pygame
+from singletonMeta import SingletonMeta
+
+
+class AssetController(metaclass=SingletonMeta):
+
+    def __init__(self):
+        self.__assets = {'player': pygame.image.load('tiles/player.png').convert_alpha(),
+                         'parede_vertical_esquerda': pygame.image.load('tiles/parede_vertical_esquerda.png').convert_alpha(),
+                         'parede_quina_direita_cima': pygame.image.load('tiles/parede_quina_direita_cima.png').convert_alpha(),
+                         'parede_quina_esquerda_baixo': pygame.image.load('tiles/parede_quina_esquerda_baixo.png'),
+                         'parede_quina_direita_baixo': pygame.image.load('tiles/parede_quina_direita_baixo.png').convert_alpha(),
+                         'parede_quina_esquerda_cima': pygame.image.load('tiles/parede_quina_esquerda_cima.png').convert_alpha(),
+                         'parede': pygame.image.load('tiles/parede.png').convert_alpha(),
+                         'parede_com_corrente': pygame.image.load('tiles/parede_com_corrente.png').convert_alpha(),
+                         'parede_com_vaso': pygame.image.load('tiles/parede_com_vaso.png').convert_alpha(),
+                         'parede_vertical_direita': pygame.image.load('tiles/parede_vertical_direita.png').convert_alpha(),
+                         'parede_horizontal_cima': pygame.image.load('tiles/parede_horizontal_cima.png').convert_alpha(),
+                         'pilha': pygame.image.load('tiles/pilha.png').convert_alpha(),
+                         'porta': pygame.image.load('tiles/porta.png').convert_alpha(),
+                         'barril': pygame.image.load('tiles/barril.png').convert_alpha(),
+                         'porta_cima': pygame.image.load('tiles/porta_cima.png').convert_alpha(),
+                         'key': pygame.image.load('tiles/key.png'),
+                         'enemyhighdmg': pygame.image.load('tiles/parede_vertical_direita.png').convert_alpha(),
+                         'enemylowdmg': pygame.image.load('tiles/porta.png').convert_alpha()
+                         }
+
+    def get_asset(self, key):
+        return self.__assets[key]
