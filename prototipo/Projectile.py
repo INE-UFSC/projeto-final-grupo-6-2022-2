bf0e7b483fc2b8de73ab5dcf28be2f4d509eb02b
Guilhermeaj19 <guilhermeadenilson16@gmail.com>
2022-12-09 20:49:25 -0300

    Ajustes

--- prototipo/Projectile.py
+++ prototipo/Projectile.py
@@ -9,7 +9,7 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self, sprite, direction, damage, speed):
         super().__init__()
         self.explosion_group = pygame.sprite.Group()
-        self.__sprite = self.image = AssetController().get_asset(sprite)
+        self.__sprite = self.__image = AssetController().get_asset(sprite)
         self.__damage = damage
         self.__direction = pygame.math.Vector2()
         self.__direction.x = direction[0]
@@ -20,9 +20,15 @@ class Projectile(pygame.sprite.Sprite):
         pos = list(pos)
         pos[0] += self.__direction.x * 25
         pos[1] += self.__direction.y * 50
-        self.rect = self.image.get_rect(topleft=pos)
-        self.__hitbox = self.rect.inflate(0, -5)
+        self.__rect = self.__image.get_rect(topleft=pos)
+        self.__hitbox = self.__rect.inflate(0, -5)
 
+    def getRect(self):
+        return self.__rect
+
+    def getImage(self):
+        return self.__image
+    
     def getDamage(self):
         return self.__damage
 
